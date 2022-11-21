d31b0f8de2e52bed7cf479321fcdfbdccc628a89
Behshad RahmanPour <79779771+behshadrhp@users.noreply.github.com>
2022-11-21 18:26:07 +0330

    create serializer field [discoun, profit] in store.serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -1,8 +1,31 @@
 from rest_framework import serializers
 from .models import Product
+from decimal import Decimal
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'price', 'inventory', 'collection', 'last_update']
+        fields = ['id', 'title', 'description', 'price',
+                  'discount', 'profit', 'inventory', 'collection', 'last_update']
+
+    discount = serializers.SerializerMethodField(method_name='discount_price')
+    profit = serializers.SerializerMethodField(
+        method_name='profit_of_the_product')
+
+    def discount_price(self, product: Product):
+        if product.price <= 50.00:
+            discount = 0.13
+            discounted = product.price * Decimal(discount)
+            total = product.price - discounted
+            return round(total, 2)
+        elif product.price >= 50.00:
+            discount = 0.25
+            discounted = product.price * Decimal(discount)
+            total = product.price - discounted
+            return round(total, 2)
+
+    def profit_of_the_product(self, product: Product):
+        discount = self.discount_price(product)
+        profit = product.price - Decimal(discount)
+        return round(profit, 2)
