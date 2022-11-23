ee1c773b41521d547c4cced7153124c5199406e9
Behshad RahmanPour <79779771+behshadrhp@users.noreply.github.com>
2022-11-23 19:40:47 +0330

    create save property for store.serilaizer.ProductSerializer in views.product_list

--- store/views.py
+++ store/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
 
 # Create your views here.
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all().order_by('id')
-    serializers = ProductSerializer(queryset, many=True)
-    return Response(serializers.data)
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all().order_by('id')
+        serializers = ProductSerializer(queryset, many=True)
+        return Response(serializers.data)
+    elif request.method == 'POST':
+        serializers = ProductSerializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
 
 
 @api_view()
