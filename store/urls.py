411579572a3c203a54d6ff7af406934d39f57040
Behshad RahmanPour <79779771+behshadrhp@users.noreply.github.com>
2022-12-13 11:59:20 +0330

    create nested router in store.urls

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from . import views
 
 router = DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('Collections', views.CollectionViewSet)
 
+products_router = NestedDefaultRouter(router, 'products', lookup='product_pk')
+products_router.register('reviews', views.ReviewViewSet,
+                         basename='product-reviews')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(products_router.urls)),
 ]
