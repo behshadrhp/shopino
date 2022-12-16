fd23774a88de99228be4c864606ad2ae8bf2a2a3
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2022-12-16 17:04:35 +0330

    update

--- core/settings.py
+++ core/settings.py
@@ -90,13 +90,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'shopino',
-        'USER': 'root',
-        'PASSWORD': '9814*45*83',
-        'HOST': 'localhost',
-        'PORT': '3306',
+       'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
