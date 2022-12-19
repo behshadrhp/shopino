9a31f021e32e3bda3ca066920b8f661b036145ac
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2022-12-19 07:15:33 +0330

    create: nested url in store.urls

--- store/urls.py
+++ store/urls.py
@@ -4,13 +4,10 @@ from . import views
 
 router = DefaultRouter()
 router.register('products', views.ProductViewSet)
-router.register('Collections', views.CollectionViewSet)
+router.register('collections', views.CollectionViewSet)
 
 products_router = NestedDefaultRouter(router, 'products', lookup='product_pk')
 products_router.register('reviews', views.ReviewViewSet,
                          basename='product-reviews')
 
-urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(products_router.urls)),
-]
+urlpatterns = router.urls + products_router.urls
