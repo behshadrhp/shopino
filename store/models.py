cb17a533b266249b5cf3a6d894c32ec70a75e724
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2022-12-24 10:25:04 +0330

    update: cart & cartitem in store.blog.models

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
 
@@ -108,6 +109,7 @@ class OrderItem(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True, editable=False, unique=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -118,9 +120,12 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     quantity = models.SmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product']]
 
     def __str__(self):
         return f'{self.product} - {self.quantity} - {self.cart}'
