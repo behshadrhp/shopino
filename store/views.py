543c82b7ce6e398bc1e1d8bbca476a48a92abbcf
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2022-12-26 11:27:33 +0330

    SQL query optimization in CartViewSet

--- store/views.py
+++ store/views.py
@@ -55,5 +55,5 @@ class ReviewViewSet(ModelViewSet):
 
 
 class CartViewSet(ModelViewSet):
-    queryset = Cart.objects.all()
+    queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
