4cedc9b2c97711a7e051a51f264475effc8cbc15
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-01-30 23:07:48 +0330

    create: new user model in auth.models

--- core/settings.py
+++ core/settings.py
@@ -148,3 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
 }
+
+
+# authentication model
+AUTH_USER_MODEL = 'authentication.User'
