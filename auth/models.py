9c32ce31dc5069c62fd5371e46969db2052a6787
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-02-02 06:49:44 +0330

    create: add regex property to User model

--- auth/models.py
+++ auth/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
 
 class User(AbstractUser):
+    username_regex = RegexValidator(
+        regex='^[a-zA-Z]{6,30}$', message='The username must be between 6 and 30 characters long, and only uppercase and lowercase Latin letters are allowed.')
+    username = models.CharField(
+        max_length=30, unique=True, validators=[username_regex])
     email = models.EmailField(unique=True)
 
     REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
