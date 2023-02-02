6045196af0e90856a15961024832cfc1650b4f75
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-02-02 06:59:31 +0330

    create: add reserved username property to User.username

--- auth/models.py
+++ auth/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from json import load
 
 # Create your models here.
 
@@ -11,6 +13,17 @@ class User(AbstractUser):
     username = models.CharField(
         max_length=30, unique=True, validators=[username_regex])
     email = models.EmailField(unique=True)
+
+    def clean(self):
+        username = self.username
+        username_lower = username.lower()
+
+        with open('auth/reserved-username/username.json', 'r') as username:
+            reserved_username = load(username)
+
+        for item in reserved_username:
+            if username_lower == item:
+                raise ValidationError('Sorry, this username is not available')
 
     REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
 
