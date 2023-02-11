2a9aff9bc305958ae789bf69d94f19f8533dbc17
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-02-11 13:14:27 +0330

    fixed: OrderItem - update database

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey('Collection', on_delete=models.CASCADE)
-    promotions = models.ManyToManyField('Promotion', null=True, blank=True)
+    promotions = models.ManyToManyField('Promotion', blank=True)
 
     def __str__(self):
         return self.title
