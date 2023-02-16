0eabfc273ddec66a4ab3b21ad81b2516cd737b99
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-02-16 17:29:36 +0330

    fixed: remove get_or_create method

--- store/serializers.py
+++ store/serializers.py
@@ -174,8 +174,7 @@ class CreateOrderSerializer(serializers.Serializer):
         with atomic():
             cart_id = self.validated_data['cart_id']
 
-            (customer, create) = Customer.objects.get_or_create(
-                user_id=self.context['user_id'])
+            customer = Customer.objects.get(user_id=self.context['user_id'])
             order = Order.objects.create(customer=customer)
 
             cart_item = CartItem.objects.select_related(
