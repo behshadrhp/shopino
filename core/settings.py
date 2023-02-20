dd005a2d2ffd7f180a96aa54d453075003f136f1
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-02-20 17:12:22 +0330

    create: media url

--- core/settings.py
+++ core/settings.py
@@ -140,6 +140,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# media conf url
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
