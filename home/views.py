a71ffdefdbf817b9097b34f05c9906d4a19eb23b
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-03-04 12:18:03 +0330

    comment: send mail

--- home/views.py
+++ home/views.py
@@ -4,14 +4,15 @@ from django.core.mail import send_mail, BadHeaderError
 # Create your views here.
 
 def home(request):
-    try:
-        send_mail(
-            'hello world', 
-            'send mail is successful', 
-            'behshad.rahmanpour@gmail.com', 
-            ['roino@gmail.com']
-        )
-    except BadHeaderError:
-        pass
+    # Send mail
+    # try:
+    #     send_mail(
+    #         'hello world', 
+    #         'send mail is successful', 
+    #         'behshad.rahmanpour@gmail.com', 
+    #         ['roino@gmail.com']
+    #     )
+    # except BadHeaderError:
+    #     pass
 
     return render(request, 'src/main.html')
