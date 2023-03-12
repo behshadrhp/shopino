e1dd212ca651b231df2e67f9d2bdd98fd5161331
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-03-12 14:41:04 +0330

    conf&run: pytest

--- store/tests/test_collection.py
+++ store/tests/test_collection.py
@@ -3,8 +3,8 @@ from rest_framework import status
 
 
 class TestCreateCollection:
-    def test_if_user_is_anonymous_return_401():
+    def test_if_user_is_anonymous_return_401(self):
         client = APIClient()
-        response = client.post('store/collections/')
-        
+        response = client.post('/store/collections/', {'title': 'hello world'})
+        print(response.status_code)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
