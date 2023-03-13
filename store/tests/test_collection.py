3359055ba94c2af7ed0fe6af50c2820f14c66771
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-03-13 09:47:11 +0330

    create test for user is not admin

--- store/tests/test_collection.py
+++ store/tests/test_collection.py
@@ -6,5 +6,12 @@ class TestCreateCollection:
     def test_if_user_is_anonymous_return_401(self):
         client = APIClient()
         response = client.post('/store/collections/', {'title': 'hello world'})
-        print(response.status_code)
+        
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_return_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'hello world'})
+        
+        assert response.status_code == status.HTTP_403_FORBIDDEN
