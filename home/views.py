df6a687f63b55fe0882c9b5999cb701df28ea18f
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-03-29 00:54:50 +0330

    test: log

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from logging import getLogger
 
 # Create your views here.
 
+logger = getLogger(__name__)
 
 def home(request):
+    logger.debug('This is a debug message')
+    logger.info('This is an info message')
+    logger.warning('This is a warning message')
+    logger.error('This is an error message')
+    logger.critical('This is a critical message')
     return render(request, 'src/main.html')
