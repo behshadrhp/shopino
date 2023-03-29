59167fe2c5d4d71c986de2178be773d04d4c286e
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-03-29 18:43:30 +0330

    fix: static file dosn't exist

--- core/settings/common.py
+++ core/settings/common.py
@@ -16,7 +16,7 @@ import os
 import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
