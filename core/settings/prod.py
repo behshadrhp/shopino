f456047fbae4ef1234d610ef671156df260ac30f
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-04-03 12:23:23 +0330

    conf: database url

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,8 +1,14 @@
 import os
+import dj_database_url
 from .common import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://behshadrhp.iran.liara.run']
 
 SECRET_KEY = os.environ['secret_key']
+
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
