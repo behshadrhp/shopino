922bb7f8b7b9d56c4e0bf1c3061e6e1ed3acb11c
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-04-03 13:14:02 +0330

    udpate: prod and common

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -2,13 +2,15 @@ import os
 import dj_database_url
 from .common import *
 
-DEBUG = False
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = ['https://behshadrhp.iran.liara.run']
 
-SECRET_KEY = os.environ['secret_key']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 
 DATABASES = {
     'default': dj_database_url.config()
 }
+
+CORS_ALLOWED_ORIGINS = os.environ['CORS_ALLOWED_ORIGINS']
